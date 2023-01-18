b003bcf49df609ed253bbb4d1aed726a3422aee0
chaitanyasharma31 <c.shrma31@gmail.com>
2023-01-18 00:28:55 -0700

    tagged version 1.1

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
                     echo "building the docker image..."
                    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-                        sh 'docker build -t cshrma/demo-app:jma1.0 .'
+                        sh 'docker build -t cshrma/demo-app:jma1.1 .'
                         sh "echo $PASS | docker login -u $USER --password-stdin"
-                        sh 'docker push cshrma/demo-app:jma1.0'
+                        sh 'docker push cshrma/demo-app:jma1.1'
     }
 } 
 
